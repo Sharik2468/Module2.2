3582210d711720278ed78a88c72afe930ee6417a
Sharik2468 <nsharabanov@bk.ru>
2022-08-19 02:18:22 +0300

    Initial commit

---
+++ Module2.2/Module2.2.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+int HashFunction(string k)
+{
+	return (k.length() % 100);
+}
+void main()
+{
+	setlocale(LC_ALL, "Rus");
+	string key;
+	cout << "Ключ > "; cin >> key;
+	cout << "HashFunction(" << key << ")=" << HashFunction(key) << endl;
+	system("pause>>void");
+}
