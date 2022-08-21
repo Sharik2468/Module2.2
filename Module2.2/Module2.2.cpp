8d031aa0c769a09ad6562dbb1781ee7eea9184db
Sharik2468 <nsharabanov@bk.ru>
2022-08-21 19:33:41 +0300

    UPD 21.08

--- Module2.2/Module2.2.cpp
+++ Module2.2/Module2.2.cpp
@@ -1,14 +1,43 @@
 ﻿#include <iostream>
+#include <unordered_set>
 using namespace std;
+
+typedef unordered_set<string> Myset;
+
 int HashFunction(string k)
 {
-	return (k.length() % 100);
+    return (k.length() % 100);
 }
+
+namespace std {
+	template<>
+	class hash<string> {
+	public:
+		size_t operator()(const string& s) const
+		{
+			return (s.length() % 100);
+		}
+	};
+}
+
 void main()
 {
+	string enter = "y";
 	setlocale(LC_ALL, "Rus");
-	string key;
-	cout << "Ключ > "; cin >> key;
-	cout << "HashFunction(" << key << ")=" << HashFunction(key) << endl;
+	while (enter == "y") 
+	{
+		Myset c1;
+		string key;
+		cout << "Ключ > "; cin >> key;
+		//cout << "HashFunction(" << key << ")=" << HashFunction(key) << endl;
+		c1.insert(key);
+		Myset::hasher hfn = c1.hash_function();
+
+		std::cout << "HashFunction(" << key << ")=" << hfn(key) << std::endl;
+		std::cout << *c1.find(key); 
+
+		cout << std::endl;
+		cin >> enter;
+	}
 	system("pause>>void");
 }
